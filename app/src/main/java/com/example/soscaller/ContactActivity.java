8a3ac952c028aaf0e1f6aac58f98eb1818af6e2d
Harshit Pandey <harshitpandey902@gmail.com>
2021-07-01 19:04:26 +0530

    added contact sample card

--- app/src/main/java/com/example/soscaller/ContactActivity.java
+++ app/src/main/java/com/example/soscaller/ContactActivity.java
@@ -1,14 +1,37 @@
 package com.example.soscaller;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ContactActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActionBar bar = getSupportActionBar();
+        assert bar != null;
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffcdd0")));
+        bar.setTitle("Contacts");
+        bar.setDisplayHomeAsUpEnabled(true);
+
         setContentView(R.layout.activity_contact);
+
+
+
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/soscaller/MainActivity.java
+++ app/src/main/java/com/example/soscaller/MainActivity.java
@@ -1,22 +1,35 @@
 package com.example.soscaller;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton sosButton, contactButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-
         setContentView(R.layout.activity_main);
+
+        sosButton = findViewById(R.id.sos_button);
+        contactButton = findViewById(R.id.contact_button);
+
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
